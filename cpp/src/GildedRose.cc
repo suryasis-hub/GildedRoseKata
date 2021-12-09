27f5525e5c3c61232ece41ad36797c38f473aeb1
Suryasis <suryasishirokpaul@gmail.com>
2021-12-09 19:15:25 +0530

    Removing increment,decrement

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -12,22 +12,22 @@ void GildedRose::updateQuality()
         if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"  \
             && item.name != "Sulfuras, Hand of Ragnaros" && item.quality > 0)
         {
-            item.quality = item.quality - 1;
+            item.quality--;
         }
         else
         {
             if (item.quality < 50)
             {
-                item.quality = item.quality + 1;
+                item.quality++;
 
                 if (item.name == "Backstage passes to a TAFKAL80ETC concert" &&
                     item.sellIn < 11 && item.quality < 50)
                 {
-                    item.quality = item.quality + 1;
+                    item.quality++;
 
                     if (item.sellIn < 6 && item.quality < 50)
                     {
-                        item.quality = item.quality + 1;   
+                        item.quality++;   
                     }
                 }
             }
@@ -35,7 +35,7 @@ void GildedRose::updateQuality()
 
         if (item.name != "Sulfuras, Hand of Ragnaros")
         {
-            item.sellIn = item.sellIn - 1;
+            item.sellIn--;
         }
 
         if (item.sellIn < 0)
@@ -44,7 +44,7 @@ void GildedRose::updateQuality()
             {
                 if (item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros" && item.quality > 0)
                 {
-                    item.quality = item.quality - 1;
+                    item.quality--;
                 }
                 else
                 {
@@ -55,7 +55,7 @@ void GildedRose::updateQuality()
             {
                 if (item.quality < 50)
                 {
-                    item.quality = item.quality + 1;
+                    item.quality = item.quality++;
                 }
             }
         }
