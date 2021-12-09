2c59e6b08d51994f4684710fd01b56df08600371
Suryasis <suryasishirokpaul@gmail.com>
2021-12-09 19:12:42 +0530

    Decreasing nesting by combining

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -40,21 +40,15 @@ void GildedRose::updateQuality()
 
         if (item.sellIn < 0)
         {
-            if (item.name != "Aged Brie")
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" )
             {
-                if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros" && item.quality > 0)
                 {
-                    if (item.quality > 0)
-                    {
-                        if (item.name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+                    item.quality = item.quality - 1;
                 }
                 else
                 {
-                    item.quality = item.quality - item.quality;
+                    item.quality = 0;
                 }
             }
             else
